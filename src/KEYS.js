338f95cedcf783e74af225dea8b24b5593885ffc
Lucem Anb <lucematopz@gmail.com>
2022-01-20 12:02:37 +0300

    automatic deploys

---
+++ src/KEYS.js
@@ -0,0 +1,6 @@
+export const config = {
+    apiKey: "AIzaSyDRtz2aBwnDGFq8t0yTR4yH4YjpGif-3S4",
+    authDomain: "prowealthmanager.firebaseapp.com",
+    projectId: "prowealthmanager"
+}
+//github: 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
