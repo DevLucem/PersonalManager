85c477ffbc8426a2ea24b1cc565cf29f46545d19
Lucem Anb <lucematopz@gmail.com>
2022-04-08 09:45:24 +0300

    init functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin")
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const FIELD_VALUE = admin.firestore.FieldValue
+const FIRESTORE = admin.firestore();
+const USERS = FIRESTORE.collection('users')
+
+exports.userCreated = functions.auth.user().onCreate(user => {
+    return USERS.doc(user.uid).set({
+        name: user.displayName,
+        id: user.uid,
+        created: FIELD_VALUE.serverTimestamp(),
+        email: user.email
+    })
+})
--- src/pages/pm/components/Project.svelte
+++ src/pages/pm/components/Project.svelte
@@ -27,7 +27,7 @@
     }
 
     console.log(milestones, tasks)
-    let sharing = true; let shares = [];
+    let sharing = false; let shares = [];
     shares = project.users.slice(1);
     function shareProject(){
         let current = project.users.slice(1)
@@ -50,13 +50,15 @@
 
 
 <div class="py-8 flex flex-col justify-center">
-    <h1 class="text-center p-4 font-bold text-primary title">
+    <h1 class="text-center p-4 font-bold text-primary title flex justify-between">
         {@html project.name}
-        <button on:click={()=>sharing=true}>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-            </svg>
-        </button>
+        {#if user.uid===project.users[0]}
+            <button on:click={()=>sharing=true}>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+                </svg>
+            </button>
+        {/if}
     </h1>
     <div class="card p-4 text-gray my-4">
         <p>{project.description}</p>
