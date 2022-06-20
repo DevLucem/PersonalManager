42120af231186021dd7edf1d093a2f685861bebb
Lucem Anb <lucematopz@gmail.com>
2022-06-20 11:18:52 +0300

    added New to projects

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -4,7 +4,7 @@
     function clicked(){ dispatch('clicked',  {'click': true}) }
     export let icon = 'cancel'
     export let classes = 'w-8 h-8';
-    let icons = ['add', 'cancel', 'check', 'edit', 'refresh', 'pin', 'calendar']
+    let icons = ['add', 'cancel', 'check', 'edit', 'refresh', 'pin', 'calendar', 'home']
 </script>
 
 <button type="button" on:click={e=>{e.stopPropagation(); clicked()}} class="{classes}">
@@ -38,6 +38,10 @@
 
         {#if icon === icons[6]}
             <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+        {/if}
+
+        {#if icon === icons[7]}
+            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
         {/if}
 
     </svg>
--- src/pages/project/Home.svelte
+++ src/pages/project/Home.svelte
@@ -92,7 +92,7 @@
         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
     </svg>
 
-    <Icon icon="add" classes="w-12 h-12 -mt-8 hover:text-primary bg-transparent text-fade dark:text-accent" on:clicked={()=>doc={type: 'project'}}/>
+    <Icon icon="home" classes="w-12 h-12 -mt-8 text-primary bg-transparent text-fade dark:text-accent"/>
 
     <Icon icon="calendar" classes="w-8 h-8 hover:text-primary text-fade  dark:text-accent" on:clicked={()=>calendar='all'}/>
 
